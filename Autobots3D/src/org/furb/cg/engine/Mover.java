c0260e90c2cace7656ea3c7130ef231157658409
tschleuss <tschleuss@5273ff5c-b740-d367-91eb-b155301dc151>
2010-05-19 22:10:59 +0000

    Novas classes do projeto Autobots2D

---
+++ Autobots3D/src/org/furb/cg/engine/Mover.java
@@ -0,0 +1,18 @@
+package org.furb.cg.engine;
+
+import java.io.Serializable;
+
+public class Mover implements Serializable {
+
+	private static final long serialVersionUID = -2629552755055708433L;
+	
+	private int type;
+	
+	public Mover(int type) {
+		this.type = type;
+	}
+	
+	public int getType() {
+		return type;
+	}
+}
---
+++ Autobots3D/src/org/furb/cg/util/TipoTerreno.java
@@ -0,0 +1,24 @@
+package org.furb.cg.util;
+
+public enum TipoTerreno {
+
+	GRASS (0),
+	WATER (1),
+	TREES (2), 
+	ROBOT (3);
+	
+	private int type;
+	
+	TipoTerreno(int c){
+		setType(c);
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	public int getType() {
+		return type;
+	}
+	
+}
